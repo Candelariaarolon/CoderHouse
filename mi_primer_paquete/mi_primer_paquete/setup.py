05e2622e732df7b6ee75242cdcccc9386db04d08
Candelariaarolon <141864957+Candelariaarolon@users.noreply.github.com>
2023-10-09 20:52:04 -0300

    Add files via upload

---
+++ mi_primer_paquete/mi_primer_paquete/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mi_primer_paquete',
+    version='0.1',
+    packages=['mi_primer_paquete'],
+    author='Candelaria',
+    author_email='candeerolonn@gmail.com',
+  
+)
